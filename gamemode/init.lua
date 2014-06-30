a6bcb753095d211d5b87cd267b799f21743edd65
Dimitar Dimitrov <newbrict@gmail.com>
2014-06-30 03:23:48 -0400

    added test for player hunter, and fixed init include issue ( alliteration yo )

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -10,9 +10,14 @@ for _, addDir in pairs(csDirs) do
 	print( "-- " .. addDir )
 	local csFiles, _ = file.Find( "gamemodes/ObjHunt/gamemode/"..addDir.."/*", "MOD" )
 	for _, csFile in ipairs(csFiles) do
+		include( addDir.."/"..csFile )
 		AddCSLuaFile( addDir.."/"..csFile )
-	print( " + " .. csFile )
+		print( " + " .. csFile )
 	end
+end
+
+function GM:PlayerInitialSpawn( ply )
+	player_manager.SetPlayerClass( ply, "player_hunter" )
 end
 
 include( "shared.lua" )
