c24a45b0d2e8df00c46155f201fad75d0444f9ed
Dimitar Dimitrov <newbrict@gmail.com>
2014-06-30 01:41:29 -0400

    added cslua adder, added preliminary prop class

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,4 +1,18 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
+local csDirs = { "player_class" }
+
+print( "Adding Client Side Lua Files..." )
+
+
+for _, addDir in pairs(csDirs) do
+	print( "-- " .. addDir )
+	local csFiles, _ = file.Find( "gamemodes/ObjHunt/gamemode/"..addDir.."/*", "MOD" )
+	for _, csFile in ipairs(csFiles) do
+		AddCSLuaFile( addDir.."/"..csFile )
+	print( " + " .. csFile )
+	end
+end
+
 include( "shared.lua" )
---
+++ gamemode/player_class/player_prop.lua
@@ -0,0 +1,26 @@
+DEFINE_BASECLASS( "player_default" )
+
+local PLAYER = {} 
+
+--
+-- See gamemodes/base/player_class/player_default.lua for all overridable variables
+--
+
+PLAYER.DisplayName      = "Prop"
+PLAYER.WalkSpeed        = 200
+PLAYER.RunSpeed         = 400
+PLAYER.CanUseFlashlight = false
+PLAYER.UseVMHands       = false
+PLAYER.AvoidPlayers     = false
+PLAYER.MaxHealth        = 100
+PLAYER.DuckSpeed        = 0.1
+PLAYER.UnDuckSpeed      = 0.1
+
+
+function PLAYER:Loadout()
+
+	self.Player:RemoveAllAmmo()
+
+end
+
+player_manager.RegisterClass( "player_prop", PLAYER, "player_default" )
