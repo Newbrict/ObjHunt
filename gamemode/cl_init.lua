f06d98d25327497074685ba6efabaef1bf5a474b
Dimitar Dimitrov <newbrict@gmail.com>
2014-07-01 03:24:27 -0400

    added thirdperson

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -9,4 +9,35 @@ local function test()
 end
 	
 
+-- third person stuff
+function ThirdPersonCalcView(ply, pos, angles, fov)
+	local class = player_manager.GetPlayerClass( ply )
+	if( class != "player_prop" ) then
+		return
+	end
+	local View = {}
+	local Trace = {}
+	local AddToPlayer = Vector(0, 0, 60)
+	View.angles = angles + Angle( 0, 0, 0 )
+	View.fov = fov
+	
+	Distance = 150
+	
+	Trace.start = ply:GetPos() + AddToPlayer
+	
+	Trace.endpos = Trace.start + View.angles:Forward() * -Distance
+	Trace.mask = MASK_SOLID_BRUSHONLY
+	tr = util.TraceLine(Trace)
+	
+	if(tr.Fraction < 1) then
+		Distance = Distance * tr.Fraction
+	end
+	
+	View.origin = Trace.start + View.angles:Forward() * -Distance * 0.95
+	return View
+end
 
+hook.Add("CalcView", "Thirdperson CalcView", ThirdPersonCalcView)
+hook.Add("ShouldDrawLocalPlayer", "Draw Local Player For Thirdperson", function(ply)
+	return player_manager.GetPlayerClass( ply ) == "player_prop"
+end )
