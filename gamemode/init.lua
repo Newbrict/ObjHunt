6ccad9af9589d24c7aefb25b32bd2b8d2da9ea15
Dimitar Dimitrov <newbrict@gmail.com>
2014-07-15 14:36:14 -0400

    stripped

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -24,16 +24,3 @@ function GM:CreateTeams( )
 	team.SetSpawnPoint( TEAM_SPECTATOR, {"info_player_counterterrorist", "info_player_combine", "info_player_deathmatch", "info_player_axis"} )
 
 end
-
---[[ All network strings should be precached HERE ]]--
-hook.Add( "Initialize", "Precache all network strings", function()
-	util.AddNetworkString( "Map Time" )
-	util.AddNetworkString( "Prop Update" )
-	util.AddNetworkString( "Reset Prop" )
-	util.AddNetworkString( "round Time" )
-	util.AddNetworkString( "Selected Prop" )
-	util.AddNetworkString( "Prop Angle Lock" )
-	util.AddNetworkString( "Prop Angle Lock BROADCAST" )
-	util.AddNetworkString( "Prop Angle Snap" )
-	util.AddNetworkString( "Prop Angle Snap BROADCAST" )
-end )
