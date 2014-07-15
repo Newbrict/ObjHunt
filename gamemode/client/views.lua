5c3d7704d294b2b0b332b2ffe4a6c12d8b77023c
Dimitar Dimitrov <newbrict@gmail.com>
2014-07-15 14:22:49 -0400

    still happening?

--- gamemode/client/views.lua
+++ gamemode/client/views.lua
@@ -1,36 +1,3 @@
---[[ Do thirdperson, view heights, etc ]]--
-hook.Add("CalcView", "ObjHunt CalcView", function( ply, pos, angles, fov )
-	local view = {}
-	view.angles = angles
-	view.fov = fov
-	view.drawviewer = ply.wantThirdPerson
-
-	if( ply.wantThirdPerson ) then
-		local trace = {}
-		local addToPlayer = Vector(0, 0, ply.propHeight)
-		local viewDist = THIRDPERSON_DISTANCE
-		
-		trace.start = ply:GetPos() + addToPlayer
-		trace.endpos = trace.start + view.angles:Forward() * -viewDist
-		trace.mask = MASK_SOLID_BRUSHONLY
-		tr = util.TraceLine(trace)
-		
-		-- efficient check when not hitting walls
-		if(tr.Fraction < 1) then
-			viewDist = viewDist * tr.Fraction
-		end
-		
-		view.origin = trace.start + view.angles:Forward() * -viewDist
-		ply.viewOrigin = view.origin
-		return view
-	elseif( ply:Team() == TEAM_PROPS ) then
-		local height = math.max( ply.propHeight, VIEW_MIN_Z )
-		view.origin = ply:GetPos() + Vector(0,0, height)
-		ply.viewOrigin = view.origin
-		return view
-	end
-end )
-
 local function stencilColor( ply, ent )
 	-- make sure we're living props
 	if( !ply:Alive() || ply:Team() != TEAM_PROPS ) then return false end
@@ -77,19 +44,4 @@ hook.Add( "PreDrawHalos", "Selectable Prop Halos", function()
 	local sColor = stencilColor( LocalPlayer(), prop ) 
 	if( !sColor ) then return end
 	halo.Add( {prop}, sColor, 3, 3, 1 )
-end )
-
---[[ when the player tries to select a prop ]]--
-hook.Add( "PlayerTick", "New Player Use", function( ply )
-	if( ply:Team() != TEAM_PROPS ) then return end
-	if( ply:KeyPressed( IN_USE ) ) then
-		if( !ply.lastPropChange || os.time() - ply.lastPropChange < PROP_CHOOSE_COOLDOWN ) then return end
-		local prop = getViewEnt( ply )
-		local sColor = stencilColor( LocalPlayer(), prop ) 
-		if( sColor == GOOD_HOVER_COLOR) then
-			net.Start( "Selected Prop" )
-				net.WriteEntity( prop )
-			net.SendToServer()
-		end
-	end
 end )
